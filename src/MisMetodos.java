3836b4e481c080c51d3332726dd9b59239388d94
AlexFernandezGonzalez <afernandezgonzalez@danielcastelao.org>
2021-02-12 09:10:17 +0100

    Initial commit

---
+++ src/MisMetodos.java
@@ -0,0 +1,23 @@
+/**
+ * Java Doc:
+ * Contiene métodos útiles para usar en los proyectos
+ *
+ */
+
+
+
+public class MisMetodos {
+
+    /**
+     *
+     * Método que monta un saludo
+     * @param mensaje mensaje que quiero mostrar
+     * @return cadena con el mensaje concatenado con otra String
+     */
+
+   //Lo pongo static para que no tenga que instanciarlo
+   public static String saludo(String mensaje){
+       return "Saludo : "+mensaje;
+   }
+
+}
